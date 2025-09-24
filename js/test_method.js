fa4966f463bc37408224b4bc9635fd1299a3c42d
Adam-Elmi <Adamcade123@gmail.com>
2025-09-24 12:49:05 +0300

    Update test_method.js

--- js/test_method.js
+++ js/test_method.js
@@ -4,3 +4,16 @@ console.log(regex.test(str)); // true (Waxa la helay wixii la raadinayey)
 
 let regex_2 = /Ahmed/;
 console.log(regex_2.test(str)); // false (Lama helin wixii la raadinayey)
+
+// Without global flag
+console.log(regex.test(str)); // true
+console.log(regex.test(str)); // true
+console.log(regex.test(str)); // true
+console.log(regex.test(str)); // true
+
+// With global flag
+let regex_3 = /Adam/g;
+console.log(regex_3.test(str)); // true
+console.log(regex_3.test(str)); // true
+console.log(regex_3.test(str)); // true
+console.log(regex_3.test(str)); // false
